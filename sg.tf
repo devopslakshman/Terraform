111fc770ce50a89cffc5f0e0357e4167bf27bf57
lakshman <lakshmandevops5152@gmail.com>
2026-05-16 11:56:52 +0530

    added ec2.tf, variable.tf files, sg.tf files

--- sg.tf
+++ sg.tf
@@ -1,19 +1,15 @@
-resource "aws_security_group" "demo-1-sg" {
+resource "aws_security_group" "demo_1_sg" {
   name        = "allow_all_sg"
   description = "Allow all inbound and outbound traffic"
 
-  
-
-  # Inbound - allow everything
   ingress {
     description = "Allow all inbound traffic"
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"   # -1 means all protocols
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Outbound - allow everything
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
