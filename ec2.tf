6bd940e8230d9721f15d284d8413c5dad4e0f27c
lakshman <lakshmandevops5152@gmail.com>
2026-05-16 11:45:21 +0530

    added ec2.tf, variable.tf files

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,7 @@
-resource "aws_instance" "demo_server" {
-    ami = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.demo_sg.id]
-    tags = {
-      Name= "demo-server"
-      ENV= "Dev"
-      terraform ="true"
-    }
+resource "aws_instance" "demo-1" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.demo_1_sg.id]
 
-
-
-  
+  tags = var.ec2_tags
 }
---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+  description = "Red Hat Enterprise Linux 9"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type = map(string)
+
+  default = {
+    Name        = "demo-1"
+    Project     = "Roboshop"
+    terraform   = "true"
+    Environment = "Dev"
+  }
+}
